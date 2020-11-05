14985e8151fc52ab3256f42b93eba14599d9d5ac
Karol Harumi <karol.harumi@hotmail.com>
2020-11-04 22:25:53 -0600

    Creating card component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
+import Card from './components/Card';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <div className={'Card'}>
-          <h2>Red Mangrove</h2>
-          <p>Rhizophora mangle</p>
-
-          <span className={'Image-container'} style={{backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/en/1/16/Red_mangrove-everglades_natl_park.jpg)'}}></span>
-
-          <a href="#" className={'Button'}>Show image</a>
-        </div>
-
-      </header>
+      <Card />
     </div>
   );
 }
